78d2fb70483a9996fb631db4c043f2c9825fd054
Marco Antonio <ma.souza.junior@gmail.com>
2017-09-16 11:53:12 -0300

    ENH : Refactoring

--- main.js
+++ main.js
@@ -3,119 +3,92 @@ const
     path = require("path"),
     electron = require("electron"),
     filepaths = require("./filepaths"),
-    client = require("electron-connect").client,
+    // client = require("electron-connect").client,
 
     // Module to control application life.
     app = electron.app
 
     Tray = electron.Tray,
     Menu = electron.Menu,
-    BrowserWindow = electron.BrowserWindow
-
-class MainApp {
+    BrowserWindow = electron.BrowserWindow,
 
     /**
-     * Creates an instance of MainApp.
-     * @memberof MainApp
-     */
-    constructor() {
-        this.appIcon = null
-        this.mainWindow = null
-        this.visible = false
-    }
-
-    /**
-     * Function to Run when app is ready
+     * Create App Window
      *
-     * @memberof MainApp
+     * @param {any} mainWindow
+     * @param {any} env
      */
-    appReady() {
+    createWindow = (mainWindow, env) => { // @almost_functional
+        // and load the index.html of the app.
+        mainWindow.loadURL(url.format({
+            pathname: path.join(filepaths.dest, "index.html"),
+            protocol: "file:",
+            slashes: true
+        }))
 
-        const
-            createWindow = () => {
-                // Create the browser window.
-                this.mainWindow = new BrowserWindow({
-                    width: 300,
-                    height: 150,
-                    resizable: true,
-                    show: false,
-                    frame: false,
-                })
+        // Emitted when the window is closed.
+        mainWindow.on("closed", () => {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            mainWindow = null
+        })
 
-                // and load the index.html of the app.
-                this.mainWindow.loadURL(url.format({
-                    pathname: path.join(filepaths.dest, "index.html"),
-                    protocol: "file:",
-                    slashes: true
-                }))
+        // Open the DevTools.
+        if (env == "development") {
+            mainWindow.webContents.openDevTools({ mode: "detach" })
+        }
+    },
 
-                // Emitted when the window is closed.
-                this.mainWindow.on("closed", () => {
-                    // Dereference the window object, usually you would store windows
-                    // in an array if your app supports multi windows, this is the time
-                    // when you should delete the corresponding element.
-                    this.mainWindow = null
-                })
+    /**
+     *
+     *
+     * @param {any} mainWindow
+     * @param {any} visible
+     */
+    appReady = (createWindow, windowFun, iconFun) => { // @almost_functional D:
+        return () => {
 
-                // Open the DevTools.
-                if (process.env.NODE_ENV == "development") {
-                    this.mainWindow.webContents.openDevTools({ mode: "detach" })
-                }
-            },
+            // Setup trayIcon
+            const mainWindow = windowFun()
+            // Setup trayIcon
+            const appIcon = iconFun()
 
-            toggle = () => {
-                if (this.visible) {
-                    this.mainWindow.hide()
-                } else {
-                    this.mainWindow.show()
-                }
-                this.visible = !this.visible
-            }
+            // Click Action
+            appIcon.on("click", () =>
+                mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
+            )
+            appIcon.setToolTip('This is my application.')
+            console.log("appIcon.setToolTip",appIcon.setToolTip);
 
-        // Can be changed in runtime
-        let contextMenu = Menu.buildFromTemplate([
-            { label: 'Item1', type: 'radio' },
-            { label: 'Item2', type: 'radio' },
-            { label: 'Item3', type: 'radio', checked: true },
-            { label: 'Item4', type: 'radio' }
-        ]);
+            // Build Context Menu 'right-click'
+            appIcon.setContextMenu(Menu.buildFromTemplate([
+                { label: 'Item1', click: () => console.log("Item1 clicked")  },
+                { label: 'Item2', click: () => console.log("Item2 clicked")  },
+                { label: 'Item3', click: () => console.log("Item3 clicked")  },
+                { label: 'Item4', click: () => console.log("Item4 clicked")  }
+            ]))
 
-        // Create trayIcon
-        this.appIcon = new Tray(filepaths.icon)
-        this.appIcon.on("click", toggle) // Linux just accept normal click :/
-        this.appIcon.setToolTip('This is my application.');
-        this.appIcon.setContextMenu(contextMenu);
+            // Preload window
+            createWindow(
+                // Create app window
+                mainWindow,
+                // Dev Env
+                process.env.NODE_ENV
+            )
 
-        // Preload window
-        createWindow()
+        }
     }
 
-}
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
-const mainApp = new MainApp()
-
-
-app.on("ready", mainApp.appReady)
-
-// Quit when all windows are closed.
-app.on("window-all-closed", () => {
-    // On OS X it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== "darwin") {
-        app.hide()
-    }
-})
-
-app.on("activate", () => {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (mainApp.mainWindow === null) {
-        createWindow()
-    }
-})
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
+// Start-up the App
+app.on("ready", appReady(
+    createWindow,
+    window =>new BrowserWindow({
+        width: 300,
+        height: 150,
+        resizable: true,
+        show: false,
+        frame: false, // Show Native Frame window
+    }),
+    icon => new Tray(filepaths.icon) // To be create after the app is ready
+))
